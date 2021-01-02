331241683c3b1d83e828d3d14e99096e7b03651f
MacTung <mactrantung@gmail.com>
2021-01-02 22:25:28 +0700

    init client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useEffect } from 'react';
+import EslPage from './pages/esl';
+function App() {
+  return (
+    <div className="App">
+        <EslPage />
+    </div>
+  );
+}
+
+export default App;
